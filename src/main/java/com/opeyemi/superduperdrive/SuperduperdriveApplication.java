0a5f8001e2673a5ae5840bbb8f4de6e7202462b8
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-12-24 00:18:57 +0100

    Set up application main files and pom.xml

---
+++ src/main/java/com/opeyemi/superduperdrive/SuperduperdriveApplication.java
@@ -0,0 +1,13 @@
+package com.opeyemi.superduperdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperduperdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperduperdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opeyemi/superduperdrive/SuperduperdriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opeyemi.superduperdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperduperdriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
