4f86dda85c21c62c63a2f268a8e48d59ca38e36b
OpeyemiOluwa <idrisbalikisopeyemi@gmail.com>
2020-12-24 00:26:15 +0100

    Add template files

--- src/test/java/com/opeyemi/superduperdrive/SuperduperdriveApplicationTests.java
+++ src/test/java/com/opeyemi/superduperdrive/SuperduperdriveApplicationTests.java
@@ -1,13 +1,41 @@
 package com.opeyemi.superduperdrive;
 
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SuperduperdriveApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	private WebDriver driver;
+
+	@BeforeAll
+	static void beforeAll() {
+		WebDriverManager.chromedriver().setup();
+	}
+
+	@BeforeEach
+	public void beforeEach() {
+		this.driver = new ChromeDriver();
+	}
+
+	@AfterEach
+	public void afterEach() {
+		if (this.driver != null) {
+			driver.quit();
+		}
+	}
+
 	@Test
-	void contextLoads() {
+	public void getLoginPage() {
+		driver.get("http://localhost:" + this.port + "/login");
+		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
 }
